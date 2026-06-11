41970d63a28159a5e229bc05a271f13c1b4e2d9c
Mamadz07 <muhammadilham3025@gmail.com>
2026-06-11 10:23:05 +0700

    deploy

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,9 +1,18 @@
 import { createClient } from "@libsql/client";
 import { env } from "$env/dynamic/private";
 
-console.log(env.TURSO_DATABASE_URL);
+console.log("TURSO_DATABASE_URL =", env.TURSO_DATABASE_URL);
+console.log("TURSO_AUTH_TOKEN =", env.TURSO_AUTH_TOKEN ? "ADA" : "TIDAK ADA");
+
+if (!env.TURSO_DATABASE_URL) {
+	throw new Error("TURSO_DATABASE_URL tidak ditemukan");
+}
+
+if (!env.TURSO_AUTH_TOKEN) {
+	throw new Error("TURSO_AUTH_TOKEN tidak ditemukan");
+}
 
 export const db = createClient({
-  url: env.TURSO_DATABASE_URL,
-  authToken: env.TURSO_AUTH_TOKEN
+	url: env.TURSO_DATABASE_URL,
+	authToken: env.TURSO_AUTH_TOKEN
 });
